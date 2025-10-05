682d104acbefeba7b68c64fa3508eee1b839a059
amir <amirtaki623@gmail.com>
2025-10-05 22:13:02 +0330

    part 37

--- src/main.js
+++ src/main.js
@@ -6,3 +6,6 @@ const upButton =  document.getElementById('up-btn')
 const downButton =  document.getElementById('down-btn')
 
 const sliderLenght =  slidRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0;
+slidLeft.style.top = `-${(sliderLenght - 1) * 100}vh`
