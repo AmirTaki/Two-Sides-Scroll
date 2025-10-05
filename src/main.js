df3fa2b766dec8a55f16fded3049671515e8f806
amir <amirtaki623@gmail.com>
2025-10-05 22:26:52 +0330

    complete project

--- src/main.js
+++ src/main.js
@@ -12,30 +12,29 @@ slidLeft.style.top = `-${(sliderLenght - 1) * 100}vh`
 
 
 const nextSlide = (params) => {
+
     const sliderHeight =  sliderContainer.clientHeight;
 
     if(params === "up") {
-        activeSlideIndex ++;
+        activeSlideIndex++;
         if(activeSlideIndex > sliderLenght - 1){
             activeSlideIndex = 0
         }
-        if (params === 'down'){
-            activeSlideIndex --;
-            if(activeSlideIndex < 0){
-                activeSlideIndex = sliderLenght - 1
-            }
+    }
+    if (params === 'down'){
+        activeSlideIndex--;
+        if(activeSlideIndex < 0){
+            activeSlideIndex = sliderLenght - 1
         }
     }
+    
 
-    slidRight.style.transform = `translateY(-${
-        activeSlideIndex * sliderHeight 
-    }px)`
+    slidRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight }px)`
 
-    slidLeft.style.transform = `translateY(${
-        activeSlideIndex * sliderHeight 
-    }px)`}
+    slidLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 
+}
 downButton.addEventListener("click", () => nextSlide ('up'))
-upButton.addEventListener("click", () => nextSlide ('down'))
+upButton.addEventListener("click", () => nextSlide('down'))
 
 
