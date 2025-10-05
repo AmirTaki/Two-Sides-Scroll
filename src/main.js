b6c59ee52ba27cf14c7d5926c3948b329be232da
amir <amirtaki623@gmail.com>
2025-10-05 22:12:08 +0330

    part 36

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const sliderContainer = document.querySelector('.slider-container')
-const rightSlid = document.querySelector('.right-slid')
-const leftSlid = document.querySelector('.left-slid')
+const slidRight = document.querySelector('.right-slid')
+const slidLeft = document.querySelector('.left-slid')
 
 const upButton =  document.getElementById('up-btn')
 const downButton =  document.getElementById('down-btn')
+
+const sliderLenght =  slidRight.querySelectorAll('div').length
